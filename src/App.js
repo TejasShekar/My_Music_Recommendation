491d245f3cf7a70b32d7dde0bde54a10780e985b
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-08 11:37:17 +0530

    Update app.js added test recommendations

--- src/App.js
+++ src/App.js
@@ -1,4 +1,28 @@
 import "./styles.css";
+import React, { useState } from "react";
+
+const music = [
+  {
+    name: "Alan Walker - Faded (Lost Stories Remix) ",
+    rating: "4.7",
+    src: "https://www.youtube.com/watch?v=sv81BgWl2U0"
+  }
+];
+
+const food = [
+  {
+    name: "Gobi Manchurian",
+    rating: "4.7"
+  }
+];
+
+const travelPlaces = [
+  {
+    name: "Maravante Beach",
+    about: "",
+    rating: "4.1"
+  }
+];
 
 export default function App() {
   return (
