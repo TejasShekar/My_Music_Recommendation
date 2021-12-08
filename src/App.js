0dd3ab4e5246580abb8a39618ec73d39ab70a8ef
Tejas Shekar <68545229+TejasShekar@users.noreply.github.com>
2021-12-08 11:19:52 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>My recommendations</h1>
+      <h3> Click one of the tabs below</h3>
+      <ul>
+        <li>Music</li>
+        <li>Food</li>
+        <li>Travel Destinations</li>
+      </ul>
+    </div>
+  );
+}
